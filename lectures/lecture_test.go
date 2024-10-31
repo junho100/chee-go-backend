07751c0b6c6dfda9017023baf963cf70c07e7e67
junho100 <bemodesty306@naver.com>
2024-10-31 14:16:49 +0900

    Test: add lecture success test

--- lectures/lecture_test.go
+++ lectures/lecture_test.go
@@ -1,7 +1,10 @@
 package lectures_test
 
 import (
+	"chee-go-backend/lectures"
 	"chee-go-backend/test"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,5 +19,13 @@ func TestGetLectures(t *testing.T) {
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 
+	responseDataByte, _ := io.ReadAll(w.Body)
+	var response lectures.GetLecturesResponse
+	err := json.Unmarshal(responseDataByte, &response)
+	if err != nil {
+		t.Errorf("Failed to unmarshal response: %v", err)
+	}
+
+	assert.Equal(t, make([]lectures.GetLecturesResponseSubject, 0), response.Subjects)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
